8a4e9f6da7216920824e78426e3a596ca58adea0
Nathan Gearing <natgearing@gmail.com>
2017-05-31 11:06:58 +1000

    Fix - Formating

--- gg-shortcodes.php
+++ gg-shortcodes.php
@@ -1,5 +1,6 @@
 <?php
-    
+    namespace Ggsc;
+
 /* Plugin Name: GG Shortcodes
  * Plugin URI: https://greengraphics.com.au
  * Description: A series of custom shortcodes.
@@ -10,157 +11,154 @@
  * Domain Path: /lang
  */
  
-if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (! defined('ABSPATH')) :
+    exit;
+endif; // Exit if accessed directly
 
-if( ! class_exists( 'ggsc' ) ) : 
+if (! class_exists('Ggsc')) :
 
-class ggsc {
+    class Ggsc
+    {
+
+
+        /*
+        *  __construct
+        *
+        *  A dummy constructor to ensure plugin is only initialized once
+        *
+        */
+
+        public function __construct()
+        {
+            /* Do nothing here */
+        } // function __construct
     
     
-	/*
-	*  __construct
-	*
-	*  A dummy constructor to ensure plugin is only initialized once
-    *
-	*/
-	
-	function __construct() {
-		
-		/* Do nothing here */
-		
-	} // function __construct
-	
-	
+        /*
+        *  initialize
+        *
+        *  The real constructor to initialize plugin
+        *
+        */
+            
+        public function initialize()
+        {
+            
+            // vars
+            $this->settings = array(
+                
+                // basic
+                'name'              => __('GG Shortcodes', 'ggsc'),
+                'version'           => '20160520',
+                            
+                // urls
+                'basename'          => plugin_basename(__FILE__),
+                'path'              => plugin_dir_path(__FILE__),
+                'dir'               => plugin_dir_url(__FILE__),
+                
+                // options
+                'show_admin'        => true,
+                'show_updates'      => true,
+                'stripslashes'      => false,
+                'local'             => true,
+                'json'              => true,
+                'save_json'         => '',
+                'load_json'         => array(),
+                'default_language'  => '',
+                'current_language'  => '',
+                'capability'        => 'manage_options',
+                'uploader'          => 'wp',
+                'autoload'          => false,
+                'l10n'              => true,
+                'l10n_textdomain'   => ''
+            );
+            
+            
+            
+            add_action('init', array($this, 'init'), 5);
+            add_action('init', array($this, 'register_shortcodes'), 5);
+            add_action('wp_enqueue_scripts', array($this, 'register_assets'), 5);
+        } // function initialize
+        
+        
+        /*
+        *  init
+        *
+        *  This function will run after all plugins and theme functions have been included
+        *
+        */
+        
+        public function init()
+        {
+            /* Do stuff */
+        } // function init
+        
+        
+        /*
+        *  register_shortcodes
+        *
+        *  This function will register all shortcodes.
+        *
+        */
+        
+        function register_shortcodes() {
+            
+            function col_shortcode( $atts, $content = null ) {
+                $a = shortcode_atts( array(
+                    'class' => 'col'
+                ), $atts );
+                
+                return "<div class='" . $a['class'] . "'>" . $content . "</div>";
+            }
+            add_shortcode( 'col', 'col_shortcode' );
+
+            
+            
+        } // function register_shortcodes
+        
+        
+        /*
+        *  register_assets
+        *
+        *  This function will register scripts and styles
+        *
+        */
+        
+        function register_assets() {
+            
+            wp_register_style( 'ggsc-main', plugin_dir_url( __FILE__ ) . 'assets/css/ggsc-main.css', [], $version );
+            
+            wp_enqueue_style( 'ggsc-main' );
+        } // function register_assets
+    } // class ggsc
+
+
     /*
-	*  initialize
-	*
-	*  The real constructor to initialize plugin
-	*
-	*/
-		
-	function initialize() {	 
-        
-        // vars
-		$this->settings = array(
-			
-			// basic
-			'name'				=> __('GG Shortcodes', 'ggsc'),
-			'version'			=> '20160520',
-						
-			// urls
-			'basename'			=> plugin_basename( __FILE__ ),
-			'path'				=> plugin_dir_path( __FILE__ ),
-			'dir'				=> plugin_dir_url( __FILE__ ),
-			
-			// options
-			'show_admin'		=> true,
-			'show_updates'		=> true,
-			'stripslashes'		=> false,
-			'local'				=> true,
-			'json'				=> true,
-			'save_json'			=> '',
-			'load_json'			=> array(),
-			'default_language'	=> '',
-			'current_language'	=> '',
-			'capability'		=> 'manage_options',
-			'uploader'			=> 'wp',
-			'autoload'			=> false,
-			'l10n'				=> true,
-			'l10n_textdomain'	=> ''
-		);
-        
-        
-        
-		add_action('init',	array($this, 'init'), 5);
-		add_action('init',	array($this, 'register_shortcodes'), 5);        
-		add_action('wp_enqueue_scripts',	array($this, 'register_assets'), 5);
-	
-	} // function initialize
-	
-	
-	/*
-	*  init
-	*
-	*  This function will run after all plugins and theme functions have been included
-	*
-	*/
-	
-	function init() {
-    
-        /* Do stuff */
-    
-    } // function init
-    
-    
-    /*
-    *  register_shortcodes
+    *  ggsc
     *
-    *  This function will register all shortcodes.
+    *  The main function responsible for returning the one true ggsc Instance to functions everywhere.
+    *  Use this function like you would a global variable, except without needing to declare the global.
+    *
+    *  Example: <?php $ggsc = ggsc(); ?>
     *
     */
-    
-    function register_shortcodes() {
+
+    function ggsc()
+    {
+
+        global $ggsc;
         
-        function col_shortcode( $atts, $content = null ) {
-            $a = shortcode_atts( array(
-                'class' => 'col'
-            ), $atts );
+        if (!isset($ggsc)) {
+            $ggsc = new Ggsc();
             
-        	return "<div class='" . $a['class'] . "'>" . $content . "</div>";
+            $ggsc->initialize();
         }
-        add_shortcode( 'col', 'col_shortcode' );        
         
-    } // function register_shortcodes
-    
-    
-	/*
-	*  register_assets
-	*
-	*  This function will register scripts and styles
-	*
-	*/
-	
-	function register_assets() {
-    	
-		wp_register_style( 'ggsc-main', plugin_dir_url( __FILE__ ) . 'assets/css/ggsc-main.css', [], $version );
-		
-		wp_enqueue_style( 'ggsc-main' );	
-	
-	} // function register_assets
-    
-    
-} // class ggsc
+        return $ggsc;
+    }
 
 
-/*
-*  ggsc
-*
-*  The main function responsible for returning the one true ggsc Instance to functions everywhere.
-*  Use this function like you would a global variable, except without needing to declare the global.
-*
-*  Example: <?php $ggsc = ggsc(); ?>
-*
-*/
-
-function ggsc() {
-
-	global $ggsc;
-	
-	if( !isset($ggsc) ) {
-	
-		$ggsc = new ggsc();
-		
-		$ggsc->initialize();
-		
-	}
-	
-	return $ggsc;
-	
-}
-
-
-// initialize
-ggsc();
-
+    // initialize
+    ggsc();
 
 endif; // class exists check
